4c5c77ac6a84aa50ae52ca7e7cc13f64ba4fb1d3
Rohit Jha <r1jha@ucsd.edu>
2017-02-10 15:19:38 -0800

    combining minified js

--- combine.sh
+++ combine.sh
@@ -5,5 +5,6 @@ cat font-awesome.min.css lato.css main.css > style.css
 curl -X POST -s --data-urlencode 'input@style.css' https://cssminifier.com/raw > style.min.css
 
 cd ../js
-rm main.min.css
+rm main.min.js
 curl -X POST -s --data-urlencode 'input@main.js' https://javascript-minifier.com/raw > main.min.js
+cat jquery.min.js jquery.scrollzer.min.js jquery.scrolly.min.js skel.min.js main.min.js > script.min.js
